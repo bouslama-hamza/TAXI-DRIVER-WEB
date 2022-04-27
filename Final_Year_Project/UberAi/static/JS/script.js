8c616653829736b8c8f8fd01a559bb55e16389c2
bouslama-hamza <ham.bousa98@gmail.com>
2022-04-27 02:57:19 +0000

    Finish Animation

--- Final_Year_Project/UberAi/static/JS/script.js
+++ Final_Year_Project/UberAi/static/JS/script.js
@@ -1,13 +1,42 @@
-const image = document.getElementById("login_page_image");
+// const image = document.getElementById("login_page_image");
 
-image.style.position = 'fixed';
-image.style.left = '1300px';
 
-step = 1300 ; 
-test = setInterval(function () {
-    step -= 5;
-    image.style.left = step+"px";
-    if(step == 700){
-        clearInterval(test);
-    }
-},0.001);
+// image.style.position = 'fixed';
+// image.style.left = '1300px';
+
+// step = 1300 ; 
+// test = setInterval(function () {
+//     step -= 5;
+//     image.style.left = step+"px";
+//     if(step == 700){
+//         clearInterval(test);
+//     }
+// },0.001);
+
+function rool_left(position , step_position){
+    const image = document.getElementById("page_image");
+    image.style.position = 'fixed';
+    image.style.left = position+"px";
+    step = position ; 
+    test = setInterval(function () {
+        step -= 15;
+        image.style.left = step+"px";
+        if(step == step_position){
+            clearInterval(test);
+        }
+    },0.001);
+}
+
+function rool_right(position , step_position){
+    const image = document.getElementById("page_image");
+    image.style.position = 'fixed';
+    image.style.left = position+"px";
+    step = position ; 
+    test = setInterval(function () {
+        step += 15;
+        image.style.left = step+"px";
+        if(step == step_position){
+            clearInterval(test);
+        }
+    },0.001);
+}
